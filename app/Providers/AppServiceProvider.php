1aa865b492ee0b5a616c4d082522a8438bc58de1
Jervi <Jervi>
2021-11-29 01:56:18 +0100

    testing if secure check works fine 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultstringLength(191);
         if ($request->secure()) {
-            URL::forceScheme('https');
+            return 1;
         }
     }
 }
