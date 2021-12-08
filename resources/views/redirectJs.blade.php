4477ebdcdc2bc707e570f2f334db3d72956c8a49
jervi <Jervi>
2021-12-08 11:50:18 +0100

    added specialities, skills, badges

--- resources/views/redirectJs.blade.php
+++ resources/views/redirectJs.blade.php
@@ -1,8 +1,6 @@
 <script>
-    @if (env('APP_ENV')=='Production')
     if (location.protocol !== 'https:') {
         location.replace(`https:${location.href.substring(location.protocol.length)}`);
     }
-    @endif
 
 </script>
