558939be48e69ffa9f11f2cb952db2a8184b56c7
unknown <jervi>
2022-01-26 14:16:46 +0100

    login redirect in /

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
     <!-- Meta Data -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cirkle | Login</title>
+    <title>Maison d'entrepreneuriat | Login</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     @include('redirectJs')
