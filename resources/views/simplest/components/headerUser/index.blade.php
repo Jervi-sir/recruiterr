11321b5600e79e3a82f08ae7e49bbbf2f7ee149a
jervi <jervi>
2022-05-10 02:26:37 +0100

    edited the header

--- resources/views/simplest/components/headerUser/index.blade.php
+++ resources/views/simplest/components/headerUser/index.blade.php
@@ -1,4 +1,6 @@
 <div class="head_user">
+
+    @auth
     @include('simplest.components.headerUser.add')
 
     <!-- Message  notificiation dropdown -->
@@ -8,5 +10,16 @@
 
     <!-- profile -image -->
     @include('simplest.components.headerUser.profile')
+    @else
+    <a href="{{ route('login') }}" class="uk-button uk-button-primary quick-login">Login</a>
+    <style>
+        .quick-login {
+            border-radius: 10px;
+            background-color: #4a46fb;
+        }
+    </style>
+    @endauth
+
+
 
 </div>
